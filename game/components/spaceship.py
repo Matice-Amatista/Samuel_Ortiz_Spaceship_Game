776933056208ddbd0b02e585886379b8806375c9
Samuel Ortiz <samuortiz1305@gmail.com>
2023-07-15 01:49:58 -0500

    Second-Class

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -1,33 +1,50 @@
 import pygame
 from pygame.sprite import Sprite    
 
-from game.utils.constants import SPACESHIP, SCREEN_WIDTH, SCREEN_HEIGHT
+from game.utils.constants import SPACESHIP, SCREEN_WIDTH, SCREEN_HEIGHT,SHIP_WIDTH,SHIP_HEIGHT
 
 class Spaceship(Sprite):
+
+    X_POS = (SCREEN_WIDTH//2) - SHIP_WIDTH
+    Y_POS = 500
+    SHIP_SPEED = 10
     def __init__(self):
         self.image = SPACESHIP
-        self.image = pygame.transform.scale(self.image,(40, 60))
+        self.image = pygame.transform.scale(self.image,(SHIP_WIDTH, SHIP_HEIGHT))
         self.rect = self.image.get_rect()
-        self.rect.x = (SCREEN_WIDTH //2) - 40 
-        self.rect.y = 500
+        self.rect.x = self.X_POS  
+        self.rect.y = self.Y_POS
 
     def update(self, user_input):
         if user_input[pygame.K_LEFT]:
-            if self.rect.left > 0:
-                self.rect.x -=10
-            else:
-                self.rect.x = SCREEN_WIDTH
-        elif user_input[pygame.K_RIGHT]:
-            if self.rect.right < SCREEN_WIDTH :
-                self.rect.x +=10
-            else:
-                self.rect.x = 0
-        elif user_input[pygame.K_UP]:
-            if self.rect.y > SCREEN_HEIGHT//2:
-                self.rect.y -=10
-        elif user_input[pygame.K_DOWN]:
-            if self.rect.y < SCREEN_HEIGHT -70 :
-                self.rect.y +=10
+            self.move_left()
+        if user_input[pygame.K_RIGHT]:
+            self.move_right()
+        if user_input[pygame.K_UP]:
+                self.move_up()
+        if user_input[pygame.K_DOWN]:
+                self.move_down()
+
+
+    def move_left(self):
+        
+        self.rect.x -= self.SHIP_SPEED
+        if self.rect.x < 0:
+            self.rect.x = SCREEN_WIDTH - SHIP_WIDTH
+
+    def move_right(self):
+        self.rect.x +=  self.SHIP_SPEED
+        if self.rect.x > SCREEN_WIDTH :
+            self.rect.x = 0
+
+    def move_up(self):
+        if self.rect.y > SCREEN_HEIGHT//2:
+            self.rect.y -=self.SHIP_SPEED
+
+    def move_down(self):
+        if self.rect.y < SCREEN_HEIGHT - 70 :
+            self.rect.y +=self.SHIP_SPEED
+
 
 
 
